6ec285b365355b586341852ddc3bbeed7de3902c
naveengouda091 <bevinamaradnandishgouda@gmail.com>
2026-08-16 21:44:08 +0530

    Add detection support for all common structures (Shipwrecks, Trial Chambers, Ancient Cities, Ruined Portals, Nether Fortresses, Bastions)

--- src/main/java/com/snowmod/collector/FeatureCollector.java
+++ src/main/java/com/snowmod/collector/FeatureCollector.java
@@ -3,11 +3,10 @@ package com.snowmod.collector;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BellBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.block.entity.MobSpawnerBlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.registry.Registries;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.WorldChunk;
@@ -36,7 +35,7 @@ public class FeatureCollector {
         ChunkPos centerChunk = new ChunkPos(playerPos);
 
         int renderDistance = client.options.getViewDistance().getValue();
-        int radius = Math.min(renderDistance, 16); // Full render distance scan like findspnr
+        int radius = Math.min(renderDistance, 16);
 
         for (int dx = -radius; dx <= radius; dx++) {
             for (int dz = -radius; dz <= radius; dz++) {
@@ -54,7 +53,7 @@ public class FeatureCollector {
 
         String dimension = world.getRegistryKey().getValue().getPath();
 
-        // 1. Fast Block Entity Scanning (Spawners, Bells, End Gateways)
+        // 1. Fast Block Entity Scanning (Spawners, Bells, End Gateways, Chests)
         for (BlockEntity be : chunk.getBlockEntities().values()) {
             BlockPos pos = be.getPos();
             BlockState state = chunk.getBlockState(pos);
@@ -67,7 +66,6 @@ public class FeatureCollector {
                 FeatureData feature = new FeatureData(CollectedFeatureType.END_GATEWAY, chunkPos, pos, dimension, angle);
                 addFeature(feature);
             } else if (be instanceof BellBlockEntity || state.getBlock() instanceof BedBlock) {
-                // Village indicator block entity
                 registerStructure(CollectedFeatureType.VILLAGE, chunkPos, pos, dimension);
             }
         }
@@ -84,15 +82,56 @@ public class FeatureCollector {
                     BlockState state = chunk.getBlockState(pos);
                     Block block = state.getBlock();
 
+                    if (block == Blocks.AIR || block == Blocks.STONE || block == Blocks.DIRT || block == Blocks.GRASS_BLOCK) continue;
+
+                    // Village
                     if (block == Blocks.BELL || block == Blocks.COMPOSTER || block == Blocks.LECTERN || block == Blocks.BLAST_FURNACE || block == Blocks.SMOKER) {
                         registerStructure(CollectedFeatureType.VILLAGE, chunkPos, pos, dimension);
-                    } else if (block == Blocks.CHISELED_SANDSTONE || block == Blocks.BLUE_TERRACOTTA) {
+                    }
+                    // Shipwreck (Chests/Wood in water or beach)
+                    else if ((block == Blocks.CHEST || block == Blocks.WATER) && (chunk.getBlockState(pos.down()).isOf(Blocks.OAK_PLANKS) || chunk.getBlockState(pos.down()).isOf(Blocks.SPRUCE_PLANKS) || chunk.getBlockState(pos.down()).isOf(Blocks.DARK_OAK_PLANKS))) {
+                        registerStructure(CollectedFeatureType.SHIPWRECK, chunkPos, pos, dimension);
+                    }
+                    // Desert Pyramid
+                    else if (block == Blocks.CHISELED_SANDSTONE || block == Blocks.BLUE_TERRACOTTA) {
                         registerStructure(CollectedFeatureType.DESERT_PYRAMID, chunkPos, pos, dimension);
-                    } else if (block == Blocks.PRISMARINE || block == Blocks.SEA_LANTERN) {
+                    }
+                    // Ruined Portal
+                    else if (block == Blocks.CRYING_OBSIDIAN) {
+                        registerStructure(CollectedFeatureType.RUINED_PORTAL, chunkPos, pos, dimension);
+                    }
+                    // Trial Chambers (1.21+)
+                    else if (block == Blocks.VAULT || block == Blocks.TRIAL_SPAWNER || block == Blocks.CHISELED_TUFF) {
+                        registerStructure(CollectedFeatureType.TRIAL_CHAMBERS, chunkPos, pos, dimension);
+                    }
+                    // Ancient City
+                    else if (block == Blocks.REINFORCED_DEEPSLATE || block == Blocks.SCULK_SHRIEKER) {
+                        registerStructure(CollectedFeatureType.ANCIENT_CITY, chunkPos, pos, dimension);
+                    }
+                    // Ocean Monument
+                    else if (block == Blocks.PRISMARINE || block == Blocks.SEA_LANTERN) {
                         registerStructure(CollectedFeatureType.OCEAN_MONUMENT, chunkPos, pos, dimension);
-                    } else if (block == Blocks.DARK_OAK_LOG || block == Blocks.DARK_OAK_PLANKS) {
-                        if ("overworld".equals(dimension) && y > 60) {
-                            registerStructure(CollectedFeatureType.PILLAGER_OUTPOST, chunkPos, pos, dimension);
+                    }
+                    // Jungle Temple
+                    else if (block == Blocks.MOSSY_COBBLESTONE && chunk.getBlockState(pos.up()).isOf(Blocks.CHISELED_STONE_BRICKS)) {
+                        registerStructure(CollectedFeatureType.JUNGLE_TEMPLE, chunkPos, pos, dimension);
+                    }
+                    // Nether Fortress
+                    else if (block == Blocks.NETHER_BRICKS || block == Blocks.NETHER_BRICK_FENCE) {
+                        if ("the_nether".equals(dimension)) {
+                            registerStructure(CollectedFeatureType.NETHER_FORTRESS, chunkPos, pos, dimension);
+                        }
+                    }
+                    // Bastion Remnant
+                    else if (block == Blocks.POLISHED_BLACKSTONE_BRICKS || block == Blocks.GILDED_BLACKSTONE) {
+                        if ("the_nether".equals(dimension)) {
+                            registerStructure(CollectedFeatureType.BASTION_REMNANT, chunkPos, pos, dimension);
+                        }
+                    }
+                    // End City
+                    else if (block == Blocks.PURPUR_BLOCK || block == Blocks.END_STONE_BRICKS) {
+                        if ("the_end".equals(dimension)) {
+                            registerStructure(CollectedFeatureType.END_CITY, chunkPos, pos, dimension);
                         }
                     }
                 }
